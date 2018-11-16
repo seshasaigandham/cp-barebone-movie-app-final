0ccec36661801134b6fe93edc50ad72f7d3ee470
seshasaigandham <43941814+seshasaigandham@users.noreply.github.com>
2018-11-16 19:52:55 +0530

    Update settings.py

--- cp_django_barebones/settings.py
+++ cp_django_barebones/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&9_c9!x27f&m0_(18z%(bxl1p+^h#rzbx8k#tly4bx6c+i@^+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0',
                 'movie-app-cloned.herokuapp.com',]
