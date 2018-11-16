d86f68dddf098c7b8beffbac155545efe8837287
seshasaigandham <43941814+seshasaigandham@users.noreply.github.com>
2018-11-16 19:49:30 +0530

    Update settings.py

--- cp_django_barebones/settings.py
+++ cp_django_barebones/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&9_c9!x27f&m0_(18z%(bxl1p+^h#rzbx8k#tly4bx6c+i@^+q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = 0.0.0.0
+ALLOWED_HOSTS = ['0.0.0.0',]
 
 
 # Application definition
